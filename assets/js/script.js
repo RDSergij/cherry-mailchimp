a81adaa5974c527b52ef23d6df11564a1da6ad20
RDSergij <sergijlys@gmail.com>
2015-12-01 12:13:35 +0200

    compilation and min styles and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,16 @@ jQuery(document).ready(function() {
 			$_form = jQuery(this);
 			var data = jQuery(this).serialize();
 			$_form.find('.message').hide();
+
+			var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+			if ( !regex.test($_form.find('input[type=email]').val()) ) {
+				$_form.find('.warning_message').show('slow').delay(5000).fadeOut();
+				return true;
+			}
+
 			$_form.find('input[type=email]').attr('disabled', 'disabled');
+			$_form.find('button').attr('disabled', 'disabled');
 			jQuery.post(param.ajaxurl, data,
 				function(response) {
 					if (response.status == 'success') {
@@ -19,6 +28,7 @@ jQuery(document).ready(function() {
 						$_form.find('.fail_message').show('slow').delay(5000).fadeOut();
 					}
 					$_form.find('input[type=email]').removeAttr('disabled');
+					$_form.find('button').removeAttr('disabled');
 					$_form.trigger('reset');
 				}
 			);
