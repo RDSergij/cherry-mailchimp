a40055daa5bfab2198d0a442a170d8724babf161
RDSergij <sergijlys@gmail.com>
2015-12-08 12:47:40 +0200

    Fix ajax shortcode generate

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -57,6 +57,37 @@ jQuery( document ).ready( function() {
     });
 });
 
+function genereateShortcode( target ) {
+
+    var mask      = target.data( 'input_mask' ),
+        shortcode = target.data( 'shortcode' ),
+        sType     = target.data( 'type' ),
+        $attrForm = jQuery( '.cherry-sg-popup_fields', target ),
+        atts      = $attrForm.serializeArray(),
+        attName,
+        val,
+        result;
+
+    result = '[' + shortcode;
+
+    jQuery.each( atts, function( index, val ) {
+        result += ' ' + val.name + '="' + val.value + '"';
+    });
+
+    result += ']';
+
+    if ( 'single' !== sType ) {
+        result += '[/' + shortcode + ']';
+    }
+
+    return result;
+}
+
+function pasteShortcode( target, result ) {
+    var shortcode = genereateShortcode( target );
+    result.val( shortcode );
+}
+
 function cherryMailchimpGeneratorView() {
     jQuery.post( window.cherryMailchimpParam.ajaxurl, { action: 'cherry_mailchimp_generator_view' },
         function( response ) {
@@ -65,7 +96,32 @@ function cherryMailchimpGeneratorView() {
             jQuery( '.cherry-sg-open' ).magnificPopup({
                 type: 'inline',
                 preloader: false,
-                focus: '#name'
+                focus: '#name',
+                callbacks: {
+                    open: function() {
+
+                        var resultShortcode = jQuery( '#generated-shortcode', this.content ),
+                            target          = this.content;
+
+                        // Init UI elements
+                        jQuery( window ).trigger( 'cherry-ui-elements-init', { 'target': target } );
+
+                        pasteShortcode( target, resultShortcode );
+
+                        target.on( 'change blur', function() {
+                            pasteShortcode( target, resultShortcode );
+                        });
+
+                        target.on( 'click', '.cherry-switcher-wrap', function() {
+                            pasteShortcode( target, resultShortcode );
+                        });
+
+                        jQuery( '.cherry-slider-unit' ).on( 'slidechange', function() {
+                            pasteShortcode( target, resultShortcode );
+                        } );
+
+                    }
+                }
             });
 
         });
